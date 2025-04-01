086bf4de343b8ccd8be3bc7139076d1a0d3668b1
AustinNolte <austinnolte22@gmail.com>
2025-04-01 14:26:10 -0700

    feat: redo of start level and Portal
    Portal now has text that will set to the world name that is set to teleport too. It will show up as A floating magenta text and prompting the player to press the interact key [E] in order to teleport

---
+++ Source/LearningUnrealProj/Private/Portal.cpp
@@ -0,0 +1,78 @@
+#include "Portal.h"
+
+APortal::APortal(){
+
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("InteractableArea")); // name object
+	Sphere->SetSphereRadius(100.0f); // set size
+	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly); // set to only check for overlap, not actually collide
+	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore); // ignore collision channels and masks
+	Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap); // check overlap for pawns
+	
+	RootComponent = Sphere;
+
+	DisplayText = CreateDefaultSubobject<UTextRenderComponent>(TEXT("No World"));
+	DisplayText->SetHorizontalAlignment(EHTA_Center);
+	DisplayText->SetVerticalAlignment(EVRTA_TextCenter);
+	DisplayText->SetWorldSize(50.0f);
+	DisplayText->SetTextRenderColor(FColor::Magenta);
+
+	DisplayText->SetupAttachment(RootComponent);
+
+	DisplayText->SetVisibility(false);
+}
+
+void APortal::BeginPlay() {
+	Super::BeginPlay();
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &APortal::OnOverlapBegin);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &APortal::OnOverlapEnd);
+
+	if (WorldName != "") {
+		DisplayText->SetText(FText::FromString(WorldName.ToString() + "\n Press [E] to teleport"));
+	} 
+	else {
+		DisplayText->SetText(FText::FromString("World Not Set"));
+	}
+}
+
+void APortal::Interact(AActor* InteractingActor){
+	
+	AFirstPersonCharacter* Player = Cast<AFirstPersonCharacter>(InteractingActor);
+	if (Player && WorldName != "") {
+
+		FLatentActionInfo LatentInfo;
+		UGameplayStatics::LoadStreamLevel(this, WorldName, true, false, LatentInfo);
+		UGameplayStatics::UnloadStreamLevel(this, GetWorld()->GetFName(), LatentInfo, false);	
+	}
+}
+
+
+void APortal::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult){
+
+	AFirstPersonCharacter* Player = Cast<AFirstPersonCharacter>(OtherActor);
+
+	if (Player) {
+		UE_LOG(LogTemp, Warning, TEXT("Overlap Begun with: %s"), *Player->GetName());
+	
+		DisplayText->SetVisibility(true);
+	}
+	
+
+	
+}
+
+void APortal::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) {
+
+	AFirstPersonCharacter* Player = Cast<AFirstPersonCharacter>(OtherActor);
+
+	if (Player) {
+		UE_LOG(LogTemp, Warning, TEXT("Overlap Ended with: %s"), *Player->GetName());
+	
+		DisplayText->SetVisibility(false);
+	}
+
+}
+
